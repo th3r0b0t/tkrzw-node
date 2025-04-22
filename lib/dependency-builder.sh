f252dc35d2b48414ca0ad81a2e6d71c45d702bcb
shahab.seddigh <shahab.seddigh@partsoftware.com>
2025-04-22 13:59:24 +0330

    V1.0.1 Fixed dependency-builder.sh
    - `npm` hangs forever on `sudo` password prompt without showing it; even with `--foreground-scripts`. It also ignores any `read` command. Those lines are commented out from dependency-builder.sh
    - This behavior is only for dependency packages, not the main package you run `npm i` in it.

--- lib/dependency-builder.sh
+++ lib/dependency-builder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-printf "Current working dir should be inside lib, confirm it: \033[0;31m$(pwd)\n\033[0mIf it's not, use ctrl+c to stop script:\n"
-read -p "Enter to continue or ctrl+c to stop:    "
+#printf "Current working dir should be inside lib, confirm it: \033[0;31m$(pwd)\n\033[0mIf it's not, use ctrl+c to stop script:\n"
+#read -p "Enter to continue or ctrl+c to stop:    "			#npm ignores this
 
 INSTALL_PREFIX=$(pwd)
 
@@ -15,13 +15,13 @@ wget https://dbmx.net/tkrzw/pkg/tkrzw-1.0.32.tar.gz
 tar xvf tkrzw-1.0.32.tar.gz
 rm -f tkrzw-1.0.32.tar.gz
 
-clear
+#clear									#this is bad
 
-printf "We need sudo rights to install liblz4-dev and cmake, please provide password if prompted:\n"
-sudo apt install liblz4-dev cmake
+#printf "We need sudo rights to install liblz4-dev and cmake, please provide password if prompted:\n"
+#sudo apt install liblz4-dev cmake					#npm hangs on this forever
 
-printf "\033[0;31mWe are going to build Tkrzw library, it can take several minutes;\033[0m If you aren't ready, use ctrl+c to stop script:\n"
-read -p "Enter to start building or ctrl+c to stop:    "
+printf "\033[0;31mWe are going to build Tkrzw library, it can take several minutes...\033[0m\n"
+#read -p "Enter to start building or ctrl+c to stop:    "		#npm ignores this
 
 cd tkrzw-1.0.32
 ./configure --enable-opt-native --enable-most-features --enable-lz4 --disable-shared --enable-static --prefix=$INSTALL_PREFIX
